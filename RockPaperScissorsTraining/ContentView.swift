b75f6bb9c856da55785282b3629cfc47dcd9f284
Paul Krznaric <paulkrznaric@gmail.com>
2020-06-09 03:37:20 -0400

    Initial Commit

---
+++ RockPaperScissorsTraining/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissorsTraining
+//
+//  Created by Paul Krznaric on 2020-06-09.
+//  Copyright © 2020 Krznarnetic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
